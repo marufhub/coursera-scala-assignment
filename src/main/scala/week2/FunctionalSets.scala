edfb7eb91152d653768fdf085dfdc62d35eb0070
Abdullah Al Maruf <maruf@Abdullahs-MacBook-Pro.local>
2017-01-26 14:44:55 +0900

    Week 2 Functional Set

--- src/main/scala/week2/FunctionalSets.scala
+++ src/main/scala/week2/FunctionalSets.scala
@@ -17,5 +17,33 @@ object FunctionalSets extends App{
   def diff(s1: Set, s2: Set): Set = (i: Int) => s1(i) && !s2(i)
 
   def filter(s: Set, p: Int => Boolean): Set = (i: Int) => s(i) && p(i)
-  
+
+  def forall(s: Set, p: Int => Boolean): Boolean = {
+    def iter(i: Int): Boolean = {
+      if(i > 1000) true
+      else if (contains(s, i)) p(i) && iter(i + 1)
+      else iter(i + 1)
+    }
+    iter(-1000)
+  }
+
+  def exists(s: Set, p: Int => Boolean): Boolean = {
+
+    def iter(i: Int): Boolean = {
+      if (i > 1000) false
+      else if (contains(s, i)) p(i) || iter(i + 1)
+      else iter(i + 1)
+    }
+    iter(1000)
+  }
+
+  def map(s: Set, f: Int => Int): Set = (y: Int) => {
+    def iter(i: Int): Boolean = {
+      if (i > 1000) false
+      else if (contains(s, i)) (f(i) == y) || iter(i + 1)
+      else iter(i + 1)
+    }
+    iter(-1000)
+  }
+
 }
